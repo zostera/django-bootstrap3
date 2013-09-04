6a8b9df7145f9303c42275a6e24bff2fe71699b1
Dylan Verheul <dylan@zostera.nl>
2013-09-04 22:55:42 +0200

    Fix unittest for python < 2.7

--- bootstrap3/tests.py
+++ bootstrap3/tests.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 from django.template import Template, Context
-from unittest import TestCase
+from django.utils.unittest import TestCase
 from django import forms
 
 
