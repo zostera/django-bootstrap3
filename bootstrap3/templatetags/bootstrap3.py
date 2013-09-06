2d3de6eb310ca8243f52d0d4cf997b4b8d8e78f2
Dylan Verheul <dylan@zostera.nl>
2013-09-06 12:20:01 +0200

    Ported pagination from django-bootstrap-toolkit

--- bootstrap3/templatetags/bootstrap3.py
+++ bootstrap3/templatetags/bootstrap3.py
@@ -1,3 +1,5 @@
+from math import floor
+import re
 from django import template
 
 from ..bootstrap import jquery_url, javascript_url, css_url
@@ -121,3 +123,91 @@ class BootstrapFormButtonsNode(template.Node):
             return ''
         else:
             return output
+
+@register.inclusion_tag("bootstrap3/pagination.html")
+def bootstrap_pagination(page, **kwargs):
+    """
+    Render pagination for a page
+    """
+    pagination_kwargs = kwargs.copy()
+    pagination_kwargs['page'] = page
+    return get_pagination_context(**pagination_kwargs)
+
+
+def get_pagination_context(page, pages_to_show=11, url=None, size=None, extra=None):
+    """
+    Generate Bootstrap pagination context from a page object
+    """
+    pages_to_show = int(pages_to_show)
+    if pages_to_show < 1:
+        raise ValueError("Pagination pages_to_show should be a positive integer, you specified %s" % pages_to_show)
+    num_pages = page.paginator.num_pages
+    current_page = page.number
+    half_page_num = int(floor(pages_to_show / 2)) - 1
+    if half_page_num < 0:
+        half_page_num = 0
+    first_page = current_page - half_page_num
+    if first_page <= 1:
+        first_page = 1
+    if first_page > 1:
+        pages_back = first_page - half_page_num
+        if pages_back < 1:
+            pages_back = 1
+    else:
+        pages_back = None
+    last_page = first_page + pages_to_show - 1
+    if pages_back is None:
+        last_page += 1
+    if last_page > num_pages:
+        last_page = num_pages
+    if last_page < num_pages:
+        pages_forward = last_page + half_page_num
+        if pages_forward > num_pages:
+            pages_forward = num_pages
+    else:
+        pages_forward = None
+        if first_page > 1:
+            first_page -= 1
+        if pages_back > 1:
+            pages_back -= 1
+        else:
+            pages_back = None
+    pages_shown = []
+    for i in range(first_page, last_page + 1):
+        pages_shown.append(i)
+    # Append proper character to url
+    if url:
+        # Remove existing page GET parameters
+        url = unicode(url)
+        url = re.sub(r'\?page\=[^\&]+', u'?', url)
+        url = re.sub(r'\&page\=[^\&]+', u'', url)
+        # Append proper separator
+        if u'?' in url:
+            url += u'&'
+        else:
+            url += u'?'
+    # Append extra string to url
+    if extra:
+        if not url:
+            url = u'?'
+        url += unicode(extra) + u'&'
+    if url:
+        url = url.replace(u'?&', u'?')
+    # Set CSS classes, see http://twitter.github.io/bootstrap/components.html#pagination
+    pagination_css_classes = ['pagination']
+    if size == 'small':
+        pagination_css_classes.append('pagination-sm')
+    elif size == 'large':
+        pagination_css_classes.append('pagination-lg')
+    # Build context object
+    return {
+        'bootstrap_pagination_url': url,
+        'num_pages': num_pages,
+        'current_page': current_page,
+        'first_page': first_page,
+        'last_page': last_page,
+        'pages_shown': pages_shown,
+        'pages_back': pages_back,
+        'pages_forward': pages_forward,
+        'pagination_css_classes': ' '.join(pagination_css_classes),
+    }
