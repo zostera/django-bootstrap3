77d46b1beff2b75e3db42d10fb1dadaa64e8a31e
Dylan Verheul <dylan@zostera.nl>
2013-09-06 09:49:28 +0200

    Prevent unicode error for certain byte strings, add test

--- bootstrap3/utils.py
+++ bootstrap3/utils.py
@@ -1,4 +1,10 @@
 try:
-    from django.utils.encoding import force_text
+    from django.utils.encoding import force_text as force_unicode
 except ImportError:
-    from django.utils.encoding import force_unicode as force_text
+    from django.utils.encoding import force_unicode
+
+
+def force_text(s):
+    if isinstance(s, str):
+        return s
+    return force_unicode(s)
