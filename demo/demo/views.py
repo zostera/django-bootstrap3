8f75d0d6d48c65c730e890e0fff0af2d8999e20d
Dylan Verheul <dylan@zostera.nl>
2013-10-16 08:17:31 +0200

    Added a demo app

---
+++ demo/demo/views.py
@@ -0,0 +1,46 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.views.generic import FormView
+from django.views.generic.base import TemplateView
+
+from .forms import ContactForm
+
+
+class HomePageView(TemplateView):
+    template_name = 'demo/home.html'
+
+
+class FormView(FormView):
+    template_name = 'demo/form.html'
+    form_class = ContactForm
+
+
+class FormHorizontalView(FormView):
+    template_name = 'demo/form_horizontal.html'
+    form_class = ContactForm
+
+
+class FormInlineView(FormView):
+    template_name = 'demo/form_inline.html'
+    form_class = ContactForm
+
+
+class PaginationView(TemplateView):
+    template_name = 'demo/pagination.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PaginationView, self).get_context_data(**kwargs)
+        lines = []
+        for i in range(10000):
+            lines.append(u'Line %s' % (i + 1))
+        paginator = Paginator(lines, 10)
+        page = self.request.GET.get('page')
+        try:
+            show_lines = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            show_lines = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            show_lines = paginator.page(paginator.num_pages)
+        context['lines'] = show_lines
+        return context
