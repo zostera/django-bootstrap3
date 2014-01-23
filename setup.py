afc271fa7478567051ad7304b87b17aa40bd41c4
Fabio C. Barrionuevo da Luz <bnafta@gmail.com>
2014-01-23 02:32:03 -0300

    refactoring setup.py

--- setup.py
+++ setup.py
@@ -1,90 +1,58 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function
-
-from setuptools import setup
-import re
 import os
 import sys
 
+import bootstrap3
 
-name = 'django-bootstrap3'
-package = 'bootstrap3'
-description = 'Bootstrap support for Django projects'
-url = 'https://github.com/dyve/django-bootstrap3'
-author = 'Dylan Verheul'
-author_email = 'dylan@dyve.net'
-license = 'Apache License 2.0'
-install_requires = []
-classifiers = [
-    'Development Status :: 4 - Beta',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: Apache Software License',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.6',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.3',
-    'Operating System :: OS Independent',
-    'Topic :: Software Development :: Libraries',
-    'Topic :: Utilities',
-    'Environment :: Web Environment',
-    'Framework :: Django',
-]
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-def get_version(package):
-    """
-    Return package version as listed in `__version__` in `init.py`.
-    """
-    init_py = open(os.path.join(package, '__init__.py')).read()
-    return re.search("^__version__ = ['\"]([^'\"]+)['\"]", init_py, re.MULTILINE).group(1)
-
-
-def get_packages(package):
-    """
-    Return root package and all sub-packages.
-    """
-    return [dirpath
-            for dirpath, dirnames, filenames in os.walk(package)
-            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
-
-
-def get_package_data(package):
-    """
-    Return all files under the root package, that are not in a
-    package themselves.
-    """
-    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
-            for dirpath, dirnames, filenames in os.walk(package)
-            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
-
-    filepaths = []
-    for base, filenames in walk:
-        filepaths.extend([os.path.join(base, filename)
-                          for filename in filenames])
-    return {package: filepaths}
-
+version = bootstrap3.__version__
 
 if sys.argv[-1] == 'publish':
-    os.system("python setup.py sdist upload")
-    args = {'version': get_version(package)}
+    os.system('python setup.py sdist upload')
     print("You probably want to also tag the version now:")
-    print("  git tag -a %(version)s -m 'version %(version)s'" % args)
+    print("  git tag -a %s -m 'version %s'" % (version, version))
     print("  git push --tags")
     sys.exit()
 
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
-    name=name,
-    version=get_version(package),
-    url=url,
-    license=license,
-    description=description,
-    author=author,
-    author_email=author_email,
-    packages=get_packages(package),
-    package_data=get_package_data(package),
-    install_requires=install_requires,
-    classifiers=classifiers,
+    name='django-bootstrap3',
+    version=version,
+    description="""Bootstrap support for Django projects""",
+    long_description=readme + '\n\n' + history,
+    author='Dylan Verheul',
+    author_email='dylan@dyve.net',
+    url='https://github.com/dyve/django-bootstrap3',
+    packages=[
+        'bootstrap3',
+    ],
+    include_package_data=True,
+    install_requires=[
+    ],
+    license="Apache License 2.0",
+    zip_safe=False,
+    keywords='django-bootstrap3',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+    ],
 )
