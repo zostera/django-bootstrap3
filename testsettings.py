804dce6f294009de5d911b096095d0bb78f8b671
Dylan Verheul <dylan@zostera.nl>
2017-10-27 13:36:12 +0200

    Remove django.contrib.gis from testsettings (not used)

--- testsettings.py
+++ testsettings.py
@@ -17,7 +17,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'django.contrib.gis',
 
     # We test this one
     'bootstrap3',
