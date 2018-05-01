45ce912a9ec79e9f9ca60be516d4f677b9e47fd0
Dylan Verheul <dylan@zostera.nl>
2018-05-01 07:51:21 +0200

    Remove unnecessary call to len (fixes #424)

--- bootstrap3/utils.py
+++ bootstrap3/utils.py
@@ -63,17 +63,16 @@ def parse_token_contents(parser, token):
     if len(bits) >= 2 and bits[-2] == 'as':
         asvar = bits[-1]
         bits = bits[:-2]
-    if len(bits):
-        for bit in bits:
-            match = kwarg_re.match(bit)
-            if not match:
-                raise TemplateSyntaxError(
-                    'Malformed arguments to tag "{}"'.format(tag))
-            name, value = match.groups()
-            if name:
-                kwargs[name] = parser.compile_filter(value)
-            else:
-                args.append(parser.compile_filter(value))
+    for bit in bits:
+        match = kwarg_re.match(bit)
+        if not match:
+            raise TemplateSyntaxError(
+                'Malformed arguments to tag "{}"'.format(tag))
+        name, value = match.groups()
+        if name:
+            kwargs[name] = parser.compile_filter(value)
+        else:
+            args.append(parser.compile_filter(value))
     return {
         'tag': tag,
         'args': args,
