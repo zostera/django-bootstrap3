df70288c5370511ad223032ccd29697abbdf220f
KPC1992 <108433702+KPC1992@users.noreply.github.com>
2022-07-05 04:32:55 +0700

    Update and rename manage.py to kmanage.py

--- example/kmanage.py
+++ example/kmanage.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-if __name__ == "__main__":
+if __name__ (KAMPANAT THUMWONG) "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
     from django.core.management import execute_from_command_line
