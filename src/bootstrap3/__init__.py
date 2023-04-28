9ec6f770932542a0d9b50fec18e6458f17e114e2
Dylan Verheul <dylan@dyve.net>
2023-04-28 11:09:29 +0200

    Fix version

--- src/bootstrap3/__init__.py
+++ src/bootstrap3/__init__.py
@@ -2,12 +2,4 @@ __all__ = [
     "__version__",
 ]
 
-PACKAGE_NAME = "django-bootstrap3"
-
-try:
-    from importlib.metadata import metadata
-except ImportError:
-    from importlib_metadata import metadata
-
-package_metadata = metadata(PACKAGE_NAME)
-__version__ = package_metadata["Version"]
+__version__ = "23.2dev"
